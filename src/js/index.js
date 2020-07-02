b247a17fcd37af5e789cbd7a6f57791e5991e9c9
Mic-PC <skibamichal@interia.pl>
2020-07-02 20:41:19 +0200

    importing es6 modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
-// Global app controller
-import number from './test'
 
-console.log(`Importing test! Number: ${number}. ${number} yay`);
+// no need to specify .js when importing
+import x from './models/Search'
+
+//import {add as a, multiply as m, ID} from './views/searchView'
+
+// * imports everything as a named object
+import * as searchView from './views/searchView'
+
+console.log(`Using imported functions! adding: ${searchView.add(searchView.ID, 23)}... multiplying: ${searchView.multiply(parseInt('15'), 9)}... also, ${x} test`);
---
+++ src/js/models/Search.js
@@ -0,0 +1,2 @@
+//default exports are used to export only one thing from the folder
+export default 'I am an exported string!';
